4928ba8857cd4c9733bcd3a5e10d55c5b4f0a0a5
kyle1elyk <steadkyle@gmail.com>
2022-02-06 20:19:01 -0500

    Added logs

--- src/main/java/com/kyle1elyk/jpaexample/drops/DropItemController.java
+++ src/main/java/com/kyle1elyk/jpaexample/drops/DropItemController.java
@@ -1,6 +1,7 @@
 package com.kyle1elyk.jpaexample.drops;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/drops")
+@Slf4j
 public class DropItemController {
 
     private final DropItemRepository dropItemRepository;
@@ -19,13 +21,19 @@ public class DropItemController {
     String postDrop(@RequestBody DropItem dropItem) {
         DropItem dropItemSaved = dropItemRepository.save(dropItem);
 
+        log.info("Saved {}", dropItemSaved);
+
         return "Saved " + dropItemSaved.getName() + " with ID " + dropItemSaved.getId();
     }
 
     @GetMapping("/{id}")
     DropItemDTO getDrop(@PathVariable Integer id) {
 
-        return DropItemMapper.INSTANCE.toDTO(dropItemRepository.findById(id).orElse(null));
+        DropItemDTO dropItemDTO = DropItemMapper.INSTANCE.toDTO(dropItemRepository.findById(id).orElse(null));
+
+        log.info("GET {}: {}", id, dropItemDTO);
+
+        return dropItemDTO;
     }
 
 }
